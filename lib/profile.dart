24006f85420ef6d8232b0b74c03838f4f073adff
Abinavraj5427 <abinavraj10@gmail.com>
2020-04-25 09:58:22 -0500

    Reformatted profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -24,81 +24,102 @@ class ProfileState extends State<Profile> {
     TextEditingController emailController = new TextEditingController();
     TextEditingController passwordController = new TextEditingController();
 
-    return new Scaffold
-
-      (
-      body: Card(
-
-        child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(
-                        // put the actual username through hint text here
-                          labelText: 'Username'
-
-                      ),
-
-
-                    ),
-
-                    TextFormField(
-                      decoration: InputDecoration(
-                        // put the actual username through hint text here
-                          labelText: 'Email'
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    Text('Hours Worked',style: TextStyle(fontSize: 30,color: Color.fromRGBO(192, 192, 192, 1)),),
-
-                    SizedBox(height: 30),
-
-
-                    Text('Rank',style: TextStyle(fontSize: 30,color: Color.fromRGBO(192, 192, 192, 1)),),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: RaisedButton(
-                            onPressed: _submit,
-                            child: Text("Update"),
-
-                          ),
-                        ),
-                      ],
+    return Scaffold(
+      body: Container(
+              child: ListView(
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 20, 20),
+                    child: Center(
+                        child: new Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Container(
+                                width: 190.0,
+                                height: 190.0,
+                                decoration: new BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: new DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: new NetworkImage(
+                                            "https://i.imgur.com/BoN9kdC.png")
+                                    )
+                                )),
+                          ],
+                        )
                     )
-                  ],
-                )
-            )
-        ),
-
-
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 10, 20),
+                    child:Text(
+                      'Email',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromRGBO(192, 192, 192, 1)
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'example@email.com',
+                      style: TextStyle(
+                          fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 10, 20),
+                    child:Text(
+                      'Username',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromRGBO(192, 192, 192, 1)
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'user.name',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 50, 0, 10),
+                    child: Center(
+                      child: Text(
+                        '10',
+                        style: TextStyle(
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: Center(
+                      child: Text(
+                        'Total Hours',
+                        style: TextStyle(
+                          fontSize: 20,
+                            color: Color.fromRGBO(192, 192, 192, 1)
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
       ),
-
-
-
-
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.save),
+          onPressed:(){
+            print("update");
+            _submit();
+          }
+      ),
     );
 
   }
 }
-//import 'package:flutter/material.dart';
-//
-//class Profile extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Container(
-//        child: new Center(
-//            child: new Text('Profile Page',
-//                style: new TextStyle(fontSize: 25.0, color: Colors.purple )
-//            )
-//        )
-//    );
-//  }
-//}
