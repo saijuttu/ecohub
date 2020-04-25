5b0d994f77beaee19897ce07f61243fbd3010589
Abinavraj5427 <abinavraj10@gmail.com>
2020-04-25 08:30:52 -0500

    Separated classes with files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecohub_app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ecohub_app/eventView.dart';
 
 void main() => runApp(MyApp(authState: AuthStatus.NOT_LOGGED_IN));
 
@@ -23,10 +24,10 @@ class MyApp extends StatefulWidget {
 
   // This widget is the root of your application.
   @override
-  _MyAppState createState() => _MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class MyAppState extends State<MyApp>{
   AuthStatus authState;
   BaseAuth auth;
   String userId;
@@ -43,151 +44,3 @@ class _MyAppState extends State<MyApp>{
       );
   }
 }
-
-class EventView extends StatelessWidget {
-  const EventView({
-    Key key,
-    this.auth,
-    @required this.myapp,
-  }) : super(key: key);
-
-  final BaseAuth auth;
-  final _MyAppState myapp;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-        children: <Widget>[
-          Scaffold(
-            body: ListView(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
-              children: <Widget>[
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.85,
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height * 0.85,
-                        child:  Image(
-                          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Container(
-                        alignment: Alignment.bottomCenter,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: <Color>[
-                              Colors.black.withAlpha(0),
-                              Colors.black12,
-                              Colors.black45
-                            ],
-                          ),
-                        ),
-                        child: Text(
-                          "Event Name",
-                          style: TextStyle(
-                              fontSize: 40,
-                              color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'SCROLL UP',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color.fromRGBO(192, 192, 192, 1)
-                      ),
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0,10,0, 2),
-                    child: Text(
-                      "5",
-                      style: TextStyle(
-                          fontSize: 35,
-                      ),
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0,5,0, 20),
-                    child: Text(
-                      'HOURS',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Color.fromRGBO(192, 192, 192, 1)
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    'DESCRIPTION',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Color.fromRGBO(192, 192, 192, 1)
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 40),
-                  child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    style: TextStyle(
-                        fontSize: 12,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 60),
-                  child: Text(
-                    'MAP',
-                    style: TextStyle(
-                      fontSize: 20,
-                        color: Color.fromRGBO(192, 192, 192, 1)
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: FloatingActionButton(
-              child: const Icon(Icons.cancel),
-                onPressed: (){
-                  print("Cancel");
-                }
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: FloatingActionButton(
-              child: const Icon(Icons.check),
-              onPressed: (){
-                print("Accepted");
-              }
-            ),
-          ),
-      ],
-    );
-  }
-}
-
-
-
